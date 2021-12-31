183930aded47dee86f63efe26cba43c9e586bea8
robin <robinv312@gmail.com>
2021-12-31 10:44:13 +0530

    form creation of the post

--- server/index.js
+++ server/index.js
@@ -7,8 +7,10 @@ import postRouter from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRouter);
 app.use(cors());
+
+app.use('/posts', postRouter);
+
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
