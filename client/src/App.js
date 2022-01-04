57946a919c8963776113b711bb29fcbca3bb0208
robin <robinv312@gmail.com>
2022-01-04 09:44:27 +0530

    update edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
 import Posts from './components/Posts/Posts';
@@ -14,10 +14,11 @@ import { useDispatch } from 'react-redux';
 const App = () => {
     const classes = useStyles();
     const dispatch = useDispatch();
+    const [currentId, setCurrentId] = useState(null);
 
     useEffect(() => {
         dispatch(getPosts());
-    }, [dispatch]);
+    }, [currentId,dispatch]);
 
     return (
         <Container maxWidth="lg">
@@ -29,10 +30,10 @@ const App = () => {
                 <Container>
                     <Grid container justify="space-between" alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7}>
-                            <Posts />
+                            <Posts setCurrentId={setCurrentId} />
                         </Grid>
                         <Grid item xs={12} sm={4}>
-                            <Form />
+                            <Form setCurrentId={setCurrentId} currentId={currentId} />
                         </Grid>
                     </Grid>
                 </Container>
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,25 +1,45 @@
 import postMessage from '../models/postMessage.js';
+import mongoose from 'mongoose';
 
 
 export const getPost = async (req, res) => {
-    try{
+    try {
         const postMessages = await postMessage.find();
         res.status(200).json(postMessages)
-    }catch(error){
-        res.status(404).json({message: error.message})
+    } catch (error) {
+        res.status(404).json({ message: error.message })
     }
 }
 
 export const createPost = async (req, res) => {
-    console.log("request body",req.body);
     const post = req.body;
     const newPost = new postMessage(post);
-    try{
+    try {
         await newPost.save();
         res.status(201).json(newPost);
-    } catch(error){
-        res.status(409).json({message: error.message})
+    } catch (error) {
+        res.status(409).json({ message: error.message })
 
     }
 
+}
+
+export const updatePost = async (req, res) => {
+    const { id } = req.params;
+    const { title, message, creator, selectedFile, tags } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+    await postMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+
+    res.json(updatedPost);
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    await postMessage.findByIdAndRemove(id);
+    res.json({ message: 'post deleted successfully' });
 }
