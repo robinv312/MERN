f6fff33754363c69d4a59457fd41018fdd757d84
robin <robinv312@gmail.com>
2022-01-04 12:14:05 +0530

    like post functionality implementation

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -43,3 +43,11 @@ export const deletePost = async (req, res) => {
     await postMessage.findByIdAndRemove(id);
     res.json({ message: 'post deleted successfully' });
 }
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    const post = await postMessage.findById(id);
+    const updatedPost = await postMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+    res.json(updatedPost);
+}
