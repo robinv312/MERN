afa2c61bcf38cdbf4503388b7af77b3ffe86f6e2
robin <robinv312@gmail.com>
2022-01-06 00:04:21 +0530

    authentication in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Auth from './components/Auth/Auth';
 
-import { getPosts } from './actions/posts';
-
-import memories from './images/memories.png';
-import useStyles from './styles';
-import { useDispatch } from 'react-redux';
-
-
-const App = () => {
-    const classes = useStyles();
-    const dispatch = useDispatch();
-    const [currentId, setCurrentId] = useState(null);
-
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [currentId, dispatch]);
-
-    return (
-        <Container maxWidth="lg">
-            <AppBar position="static" color="inherit" className={classes.appBar}>
-                <Typography variant="h2" align="center" className={classes.heading}>Memories</Typography>
-                <img src={memories} className={classes.image} alt="icon" height="60" />
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container className={classes.mainContainer} justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId} />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form setCurrentId={setCurrentId} currentId={currentId} />
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
-    );
-}
+const App = () => (
+  <BrowserRouter>
+    <Container maxWidth="lg">
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/auth" exact component={Auth} />
+      </Switch>
+    </Container>
+  </BrowserRouter>
+);
 
 export default App;
